7dcc5321e35258d54639973c178e78f52b9248eb
来馬大介 <kuruma0909@gmail.com>
2019-12-02 10:15:31 +0000

    pictgram rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
+  validates :password, length: { in: 8..12 }
+  validates :name, length: { in: 1..15 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+ validates :password, /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
   has_secure_password
 end
