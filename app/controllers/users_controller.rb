d8e802cdb6d66bdbe96987622b7c1cb2a6f04cd0
来馬大介 <kuruma0909@gmail.com>
2019-12-07 10:52:34 +0000

    ログイン機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  
       def new
         @user = User.new
       end
+      
       def create
         @user = User.new(user_params)
         if @user.save
@@ -11,6 +13,7 @@ class UsersController < ApplicationController
           render :new
         end
       end
+              
               private
           def user_params
            params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, length: { in: 8..12 }
-  validates :name, length: { in: 1..15 }
-  
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
- validates :password, /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+  validates :name, presence: true, length: { maximum: 15 }
+
+  EMAIL_REGEX = /[a-zA-Z0-9._+]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*/
+  validates :email, presence: true, format: { with: EMAIL_REGEX }
+
+  validates :password, length: { in: 8..32 }
+
+  PW_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
+  validates :password, presence: true, format: { with: PW_REGEX }
+
   has_secure_password
 end
