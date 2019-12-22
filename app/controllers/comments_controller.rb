ebf6a75785b4d6598dd2113aff7325a353216e84
来馬大介 <kuruma0909@gmail.com>
2019-12-22 12:11:22 +0000

    コメント機能完了しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,12 +6,13 @@ class CommentsController < ApplicationController
   
   def new
     @comment = Comment.new
+    @comment.topic_id = params[:topic_id]
   end
   
   def create
     @comment = current_user.comments.new(comment_params)
     # @comment.user_id = current_user.id
-    # @comment.topic_id = params[:topic_id]
+    
     if @comment.save
         redirect_to topics_path, success: 'コメントしました'
     else
@@ -21,6 +22,6 @@ class CommentsController < ApplicationController
   
   private
   def comment_params
-    params.require(:comment).permit(:comment,:topic_id)
+    params.require(:comment).permit(:content,:topic_id)
   end
 end
